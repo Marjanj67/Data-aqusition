0b220c0a8377de82f278ac517359090929035f41
Marjan Jannati <117945484+Marjanj67@users.noreply.github.com>
2022-12-06 10:45:20 +0330

    Update totalpage.py

--- Scrape ads details from rahnama.com/totalpage.py
+++ Scrape ads details from rahnama.com/totalpage.py
@@ -3,106 +3,99 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-# global variables read phone number
-curUrl = 0
-phoneNum = 0
-title = 0
-allData = {'a':'123'}
 
-
-# global variables for total urls
-cpageUrl = 0
-allurls = []
-finalUrls = []
-
-def mylinks(href):
-  global allurls
-  if href != None and href != "":
-    ch = '/item' in str(href)
-    if ch == True:
-      allurls.append(href)
-      return href
-
-def mylinks2(next):
-  global allurls
+def find_links(next):
   slinks =re.findall(r'[:][\d]{6}',str(next))
+  Urls = []
   for m in slinks:
     m = m.replace(":","/item/")
-    allurls.append(m)
+    m = "https://rahnama.com" + str(m)
+    Urls.append(m)
+  return Urls
+
+
+def find_urls(oldUrls):
+    u = open ("BaseUrls.txt", "r")
+    AllUrls = []
+    for i in u:
+      cpageUrl = i.removesuffix('\n')
+      weburl = requests.get(cpageUrl).text
+      soup = BeautifulSoup(weburl,'html.parser')
+      # s1 = soup.find_all(href=mylinks)
+      LinksDala = soup.find_all(id= "__NEXT_DATA__")
+      Urls = find_links(LinksDala)
+      for url in Urls:
+        CheckIn = url in oldUrls
+        if CheckIn == False :
+          AllUrls .append(url)
+    return AllUrls
+
+
+def read_old_urls():
+  u1 = open("oldUrls.txt","r")
+  oldUrls = []
+  for i in u1:
+    if i != 0:
+      i = i.removesuffix('\n')
+      oldUrls.append(i)
+  return oldUrls
+
+
+def write_to_file(UrlList,FileName):
+  Path = str(FileName)+'.txt'
+  f=open(str(Path),"w+")
+  for x in UrlList:
+    if x != 0 :
+      f.write(x+ "\r")
+
+
+def get_number_title(CurrentUrl):
+  Data = ''
+  PhoneNum = 0
+  ## i.removesuffix('\n')
+  co = requests.get(CurrentUrl).status_code
+  if  co== 200:
+    try:
+      webUrl = requests.get(CurrentUrl).text
+      soup1 = BeautifulSoup(webUrl,'html.parser')
+      p1 = soup1.find_all(attrs={"class": "eHTumH"})
+      # print(p1[0])
+      PhoneNum = re.findall(r"[0][\d]{10}|[9][\d]{9}|[\+98][\d]{12}",str(p1[0]))
+      title = soup1.select('title')[0].text
+      if len(PhoneNum) >0:
+        for pn in PhoneNum:
+            Data= str(title) + ',' + str(pn)
+    except:
+      print("Something went wrong")
+  return Data
 
 
 def main():
   global cpageUrl,allurls
-  # ------- getting urls
-  u = open ("BaseUrls.txt", "r")
-  for i in u:
-    cpageUrl = i.removesuffix('\n')
-    weburl = requests.get(cpageUrl).text
-    soup = BeautifulSoup(weburl,'html.parser')
-    s1 = soup.find_all(href=mylinks)
-    s4 = soup.find_all(id= "__NEXT_DATA__")
-    mylinks2(s4)
-
   # ------- reading old urls
-  u1 = open("oldUrls.txt","r")
-  oldUrls = set('0')
-  for i in u1:
-    if i != 0:
-      i = i.removesuffix('\n')
-      oldUrls.add(i)
+  oldUrls = read_old_urls()
+  # ------- getting urls
+  UrlList = find_urls(oldUrls)
+  UrlListUnique = set(UrlList)
+  # ------- writing urls in a file
+  FileName = 'newUrl'
+  write_to_file(UrlListUnique,FileName)
 
- # ------- writing urls in a file
-  f=open("newUrl.txt","w+")
-  allurls2 = set(allurls)
-  for x in allurls2:
-    ch = x in oldUrls
-    if x != 0 and  ch == False:
-      finalUrls.append(x)
-      f.write(x+ "\r")
 
   # ------- getting details-------------------------------------------------------
-  index = 0 
-  for i in finalUrls:
-    if i!=0:
-      curUrl = "https://rahnama.com" + i.removesuffix('\n')
-      co = requests.get(curUrl).status_code
-      if  co== 200:
-        try:
-          webUrl = requests.get(curUrl).text
-          soup1 = BeautifulSoup(webUrl,'html.parser')
-          p1 = soup1.find_all(attrs={"name": "description"})
-          # print(p1[0])
-          phoneNum = re.findall(r"[0][\d]{10}|[9][\d]{9}|[\+98][\d]{12}",str(p1[0]))
-          title = soup1.select('title')[0].text
-          allData[title]= phoneNum
-        except:
-          print("Something went wrong")
+  AllDetails = []
+  for CurrentUrl in UrlListUnique:
+    if CurrentUrl != 0:
+      TitleNumber = get_number_title(CurrentUrl)
+      if TitleNumber != '':
+        AllDetails.append(TitleNumber)
 
-        index = index + 1
-      else:
-        index = index + 1
     
   # open data file
   m = open("newNums.txt", "w", encoding="utf-8")
-  # read old numbers
-  u = open("oldNums.txt", "r", encoding="utf-8")
-  oldNumbers = []
-  for i in u:
-    if i != 0:
-      i = i.removesuffix('\n')
-      oldNumbers.append(i)
-
-  # compare and add new numbers 
-  newNumbers = []
-  for key,value in allData.items():
-    if value != "" and value != []:
-      newNumbers.append(value[0])
-
-  uniqueNum = set(newNumbers) - set(oldNumbers)
-  for j in uniqueNum:  
-    if j.startswith("09") or j.startswith("+989")or j.startswith("9"):
-      value1 = (j)
-      m.write(value1 + "\r")
+  for ad in AllDetails:
+    value = ad
+    m.write(value + "\r")
 
 if __name__ == "__main__":
   main();
